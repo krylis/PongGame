3f904608e9b3e949335e6d2e61e730f8b5a725a0
krylis <cmcshann@gmail.com>
2023-09-08 20:13:34 -0700

    Added paddle class. Created two paddles and made them move up and down when certain keys are pressed.

---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.speed("fastest")
+        self.goto(position)
+
+    def go_up(self):
+        new_position = (self.xcor(), self.ycor() + 20)
+        self.goto(new_position)
+
+    def go_down(self):
+        new_position = (self.xcor(), self.ycor() - 20)
+        self.goto(new_position)
