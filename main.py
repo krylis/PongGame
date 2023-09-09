dc4093318403b579fe1a20f599a09aa64eede6a8
krylis <cmcshann@gmail.com>
2023-09-08 20:51:29 -0700

    Added ball class. Added ball to the screen and made it move towards the wall. Added logic to make ball change directions when it collides with top/bottom wall or one of the paddles.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -15,5 +19,23 @@ screen.onkey(r_paddle.go_down, "Down")
 
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # detect collision with top and bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    # detect collision with left paddle
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
